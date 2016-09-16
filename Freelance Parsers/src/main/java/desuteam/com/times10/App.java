9c1a5079e5bfa739b3e5962ea29ca539b515b681
PC <PC@WIN-ES7B49N3SES>
2016-09-16 09:23:20 +0300

    10times parser with proxy

--- Freelance Parsers/src/main/java/desuteam/com/times10/App.java
+++ Freelance Parsers/src/main/java/desuteam/com/times10/App.java
@@ -1,6 +1,10 @@
 package desuteam.com.times10;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,42 +25,112 @@ public class App
     	
     }
     
-    public App() throws Exception {
+    public App() throws IOException {
+
+    	System.setProperty("java.net.useSystemProxies", "true");
+        System.setProperty("http.proxyHost", "64.33.135.190");
+        System.setProperty("http.proxyPort", "8080");
+    	PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter("data.csv", true)));
     	
-    	List<Organizer> organizers = getOrganizers(1);
     	
-    	Organizer organizer = organizers.get(0);
-    	setOrganizerInfo(organizer);
-    	setEventInfo(organizer.event);
     	
-    	print(organizer);
+    	for(int page = 48;page < 70;page++){
+	    	List<Organizer> organizers;
+	    	while(true){
+				try {
+					organizers = getOrganizers(page);
+			    	sleep(5000);
+					break;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	    	}
+	    	System.out.println("Page: " + page);
+	    	for(Organizer organizer : organizers){
+	    		while(true)
+			    	try {
+						setOrganizerInfo(organizer);
+				    	sleep(5000);
+						break;
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+	    		if(organizer.event != null)
+			    	while(true)
+				    	try {
+							setEventInfo(organizer, organizer.event);
+					    	sleep(5000);
+							break;
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+		    	
+		    	print(printWriter, organizer);
+		    	printWriter.flush();
+		    	System.out.println(organizer.name + organizer.event);
+	    	}
+    	}
+    	printWriter.close();
 	}
     
     
-    public void print(Organizer organizer){
+    public void sleep(int time){
+    	try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    public void print(PrintWriter pw, Organizer organizer){
     	String res = String.format("\"%s\",\"%s\",\"%s\",\"%s\"", 
     			organizer.name,
     			organizer.info,
-    			organizer.event.name,
-    			organizer.event.info);
+    			organizer.event == null? "":organizer.event.name,
+    			organizer.event == null? "":organizer.event.info);
     	
-    	System.out.println(res);
+    	pw.println(res);
     }
     
-    public void setEventInfo(Event event) throws Exception {
-    	Document doc = Jsoup.connect(event.link).get();
+    public void setEventInfo(Organizer organizer, Event event) throws Exception {
+    	Connection conn = Jsoup.connect(event.link);
+    	conn.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+    	conn.header("Upgrade-Insecure-Requests", "1");
+    	conn.header("X-Requested-With", "XMLHttpRequest");
+    	conn.header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36");
+    	conn.header("DNT", "1");
+    	conn.header("Referer", organizer.link);
+    	conn.header("Accept-Encoding", "gzip, deflate, sdch");
+    	conn.header("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
+    	conn.cookie("country", "Ukraine");
+    	conn.cookie("countryCode", "UA");
+    	Document doc = conn.get
+    			();
     	
     	Element info = doc.select("p.desc").first();
     	event.info = info.text();
     	
     }
     public void setOrganizerInfo(Organizer organizer) throws Exception {
-    	Document doc = Jsoup.connect(organizer.link).get();
+    	Connection conn = Jsoup.connect(organizer.link).timeout(10000);
+    	conn.header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+    	conn.header("Upgrade-Insecure-Requests", "1");
+    	conn.header("X-Requested-With", "XMLHttpRequest");
+    	conn.header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36");
+    	conn.header("DNT", "1");
+    	conn.header("Referer", "http://10times.com/organizers/china");
+    	conn.header("Accept-Encoding", "gzip, deflate, sdch");
+    	conn.header("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
+    	conn.cookie("country", "Ukraine");
+    	conn.cookie("countryCode", "UA");
+    	Document doc = conn.get();
     	Element info = doc.select("blockquote.box").first();
-    	organizer.info = info.text();
+    	organizer.info = info == null?"":info.text();
     	Element link = doc.select("div.maindiv h3 a").first();
-    	Event event = new Event(link.text(), "http://10times.com" + link.attr("href"));
-    	organizer.event = event;
+    	if(link != null){
+	    	Event event = new Event(link.text(), "http://10times.com" + link.attr("href"));
+	    	organizer.event = event;
+    	}
     }
     
     public List<Organizer> getOrganizers(int page) throws IOException{
@@ -64,7 +138,17 @@ public class App
     	List<Organizer> result = new ArrayList<Organizer>();
     	
     	Connection conn = Jsoup.connect("http://10times.com/organizers/china?&ajax=1&page="+page);
-    	conn.method(Method.GET);
+    	conn.timeout(10000).method(Method.GET);
+    	conn.header("Accept", "*/*");
+    	conn.header("X-Requested-With", "XMLHttpRequest");
+    	conn.header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36");
+    	conn.header("DNT", "1");
+    	conn.header("Referer", "http://10times.com/organizers/china");
+    	conn.header("Accept-Encoding", "gzip, deflate, sdch");
+    	conn.header("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
+    	conn.cookie("country", "Ukraine");
+    	conn.cookie("countryCode", "UA");
+    	
     	
     	Response response = conn.execute();
     	
